aa8092a80b9a8ba9e6efa4c844db672e0ca0996e
Andrei Kuzmin <Andreytko@mail.ru>
2026-02-08 01:24:03 +0300

    initial commit

---
+++ src/main/java/holymagic/vkpublicmanagement/VkPublicManagementApplication.java
@@ -0,0 +1,13 @@
+package holymagic.vkpublicmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkPublicManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VkPublicManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/holymagic/vkpublicmanagement/VkPublicManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package holymagic.vkpublicmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkPublicManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
