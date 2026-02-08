0cca5ece2981ebf5c5eb6806a2d94afa2a1b9ca8
Andrei Kuzmin <Andreytko@mail.ru>
2026-02-09 01:49:16 +0300

    implemented 3 wall methods: get, getById, search

--- src/main/java/holymagic/vkpublicmanagement/model/wall/Post.java
+++ src/main/java/holymagic/vkpublicmanagement/model/wall/Post.java
@@ -1,6 +1,5 @@
 package holymagic.vkpublicmanagement.model.wall;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import holymagic.vkpublicmanagement.model.wall.attachment.Attachment;
