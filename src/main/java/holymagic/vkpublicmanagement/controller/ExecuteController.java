9d6105204736e736703b7fefa8231be190fe50d5
Andrei Kuzmin <Andreytko@mail.ru>
2026-02-23 01:59:49 +0300

    implemented method to get total count in several http-requests

--- src/main/java/holymagic/vkpublicmanagement/controller/ExecuteController.java
+++ src/main/java/holymagic/vkpublicmanagement/controller/ExecuteController.java
@@ -1,6 +1,8 @@
 package holymagic.vkpublicmanagement.controller;
 
 import holymagic.vkpublicmanagement.dto.CountResponseDto;
+import holymagic.vkpublicmanagement.mapper.CountResponseMapper;
+import holymagic.vkpublicmanagement.model.exec.CountResponse;
 import holymagic.vkpublicmanagement.service.ExecuteService;
 import holymagic.vkpublicmanagement.validator.WallParamValidator;
 import jakarta.validation.constraints.NotBlank;
@@ -18,6 +20,7 @@ public class ExecuteController {
 
     private final ExecuteService executeService;
     private final WallParamValidator wallParamValidator;
+    private final CountResponseMapper countResponseMapper;
 
     @GetMapping("/count")
     public ResponseEntity<CountResponseDto> getCount(
@@ -26,7 +29,16 @@ public class ExecuteController {
             @RequestParam(required = false, defaultValue = "0") int offset
     ) {
         wallParamValidator.validateOffset(offset);
-        return ResponseEntity.ok(executeService.getCount(query, domain, offset));
+        CountResponse response = executeService.getCount(query, domain, offset);
+        return ResponseEntity.ok(countResponseMapper.toDto(response));
     }
 
+    @GetMapping("/count/total")
+    public ResponseEntity<CountResponseDto> geTotalCount(
+            @RequestParam @NotBlank(message = "query can't be empty") String query,
+            @RequestParam(required = false, defaultValue = "place_4_rest") String domain
+    ) {
+        CountResponse response = executeService.getTotalCount(query, domain);
+        return ResponseEntity.ok(countResponseMapper.toDto(response));
+    }
 }
--- src/main/java/holymagic/vkpublicmanagement/service/ExecuteService.java
+++ src/main/java/holymagic/vkpublicmanagement/service/ExecuteService.java
@@ -5,13 +5,16 @@ import holymagic.vkpublicmanagement.exception.EmptyResponseException;
 import holymagic.vkpublicmanagement.mapper.CountResponseMapper;
 import holymagic.vkpublicmanagement.model.exec.CountResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.net.URI;
+import java.time.LocalDateTime;
 
 import static holymagic.vkpublicmanagement.model.ParameterizedTypeReferences.EXEC_COUNT_REF;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ExecuteService {
@@ -22,11 +25,35 @@ public class ExecuteService {
     @Value("${exec_get_count_path}")
     private String getCountPath;
 
-    public CountResponseDto getCount(String query, String domain, int offset) {
+    public CountResponse getCount(String query, String domain, int offset) {
         URI uri = exchangeService.provideGetCountExecUri(getCountPath, query, domain, offset);
         CountResponse response = exchangeService.getDataForExec(uri, EXEC_COUNT_REF);
         validateCountResponse(response);
-        return countResponseMapper.toDto(response);
+        return response;
+    }
+
+    public CountResponse getTotalCount(String query, String domain) {
+        CountResponse firstResponse = getCount(query, domain, 0);
+        if (firstResponse.getCount() < 100) {
+            log.info("received total count in one request");
+            return firstResponse;
+        }
+        int requests = 1, offset = 100, count = 100;
+        boolean needMore = true;
+        Long firstTimestamp = firstResponse.getFirstTimestamp();
+        Long lastTimestamp = firstResponse.getLastTimestamp();
+        while (needMore) {
+            CountResponse newResponse = getCount(query, domain, offset);
+            lastTimestamp = newResponse.getLastTimestamp();
+            requests++;
+            offset+=100;
+            count+= newResponse.getCount();
+            if (newResponse.getCount() < 100) {
+                needMore = false;
+            }
+        }
+        log.info("received total count in {} requests", requests);
+        return new CountResponse(count, firstTimestamp, lastTimestamp);
     }
 
     private void validateCountResponse(CountResponse response) {
