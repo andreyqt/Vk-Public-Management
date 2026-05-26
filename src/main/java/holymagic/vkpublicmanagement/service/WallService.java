9a679ea662de2dd03c85f695df8ab358065c9e4c
Andrei Kuzmin <Andreytko@mail.ru>
2026-05-26 22:16:26 +0300

    deleted unused import

--- src/main/java/holymagic/vkpublicmanagement/service/WallService.java
+++ src/main/java/holymagic/vkpublicmanagement/service/WallService.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 
 import java.net.URI;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
