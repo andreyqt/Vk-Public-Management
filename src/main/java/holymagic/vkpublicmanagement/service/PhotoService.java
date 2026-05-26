d07afa4e33acfc7dec2867726f52c70d4157fded
Andrei Kuzmin <Andreytko@mail.ru>
2026-05-26 22:20:00 +0300

    deleted unused import

---
+++ src/main/java/holymagic/vkpublicmanagement/service/PhotoService.java
@@ -0,0 +1,16 @@
+package holymagic.vkpublicmanagement.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PhotoService {
+
+    private final ExchangeService exchangeService;
+
+
+
+}
