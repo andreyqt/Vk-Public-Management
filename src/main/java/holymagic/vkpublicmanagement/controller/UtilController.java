d0724b235b422bb3a44c0b8087b6077b824e05b6
Andrei Kuzmin <Andreytko@mail.ru>
2026-07-18 12:15:57 +0300

    problem solved, some refactoring

--- src/main/java/holymagic/vkpublicmanagement/controller/UtilController.java
+++ src/main/java/holymagic/vkpublicmanagement/controller/UtilController.java
@@ -4,16 +4,12 @@ import holymagic.vkpublicmanagement.dto.HashtagDto;
 import holymagic.vkpublicmanagement.dto.TwoLinkDto;
 import holymagic.vkpublicmanagement.util.LinkManager;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
